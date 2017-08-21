e9eae8faa7143a99b12c085ef955c8819af10f8f
sionelt <sltafengatoto@gmail.com>
2017-08-21 00:10:10 -0600

    initial flexifying and coloring keypad buttons

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,12 @@ import style from 'styles/globals.css';
 
 const App = () => {
 	const { demo, container, display, keypad, keys, aKey, operators, aOperator } = style;
+	const KEYS = ['C', '<', '%', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '()', '.'];
+	const OPERATORS = ['/', 'X', '-', '+', '='];
 	return (
 		<div>
 			<Helmet>
-				<title>sionelt site</title>
-				<link href={favicon} rel="shortcut icon" type="image/png" />
+				<title>improved clone iphone calculator</title>
 				<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500" rel="stylesheet" />
 				<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 			</Helmet>
@@ -17,19 +18,18 @@ const App = () => {
 					<div className={display}>123</div>
 					<div className={keypad}>
 						<ul className={keys}>
-							{keys.map(key =>
+							{KEYS.map(key =>
 								<li key={key} className={aKey}>
-									key
+									{key}
 								</li>
 							)}
 						</ul>
 						<ul className={operators}>
-							{operators.map(op =>
+							{OPERATORS.map(op =>
 								<li key={op} className={aOperator}>
-									op
+									{op}
 								</li>
 							)}
-							<li>hellow</li>
 						</ul>
 					</div>
 				</div>
@@ -39,6 +39,3 @@ const App = () => {
 };
 
 export default App;
-
-const keys = ['C', '<', '%', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '()', '.'];
-const operators = ['/', 'X', '-', '+', '='];
