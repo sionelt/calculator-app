476c19455f5cd02ada57fccb7a446f821b211986
sionelt <sltafengatoto@gmail.com>
2017-08-21 12:01:01 -0600

    handle props into Keypad from App class module

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,34 @@ import style from 'styles/globals.css';
 import Keypad from './Keypad';
 import Screen from './Screen';
 
-const App = () => {
-	const { demo, container } = style;
-
-	return (
-		<div className={demo}>
-			<Helmet>
-				<title>improved clone iphone calculator</title>
-				<link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300" rel="stylesheet" />
-			</Helmet>
-			<div className={container}>
-				<Screen />
-				<Keypad />
+class App extends Component {
+	constructor() {
+		super();
+		this.state = { keyEntered: '', displayAnEntry: '', displayAllEntries: '' };
+		this.handleClick = this.handleClick.bind(this);
+	}
+  handleClick() {
+    this.setState ({
+      keyEntered: { operator }
+    })
+  }
+	render() {
+		const { demo, container } = style;
+		const INPUTS = ['C', '<', '%', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '()', '.'];
+		const OPERATORS = ['/', 'x', '-', '+', '='];
+		return (
+			<div className={demo}>
+				<Helmet>
+					<title>improved clone iphone calculator</title>
+					<link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300" rel="stylesheet" />
+				</Helmet>
+				<div className={container}>
+					<Screen />
+          <Keypad allInputs={INPUTS} allOperators={OPERATORS} onEnter={this.handleClick} />
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import style from 'styles/Keypad.css';
 
-const Keypad = () => {
+const Keypad = props => {
 	const { container, inputs, aInput, operators, aOperator } = style;
-	const INPUTS = ['C', '<', '%', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '()', '.'];
-	const OPERATORS = ['/', 'x', '-', '+', '='];
+	const { allInputs, allOperators, onInput } = this.props;
 	return (
 		<div className={container}>
 			<ul className={inputs}>
-				{INPUTS.map(input =>
+				{allInputs.map(input =>
 					<li key={input} className={aInput}>
 						{input}
 					</li>
 				)}
 			</ul>
 			<ul className={operators}>
-				{OPERATORS.map(operator =>
-					<li key={operator} className={aOperator}>
+				{allOperators.map(operator =>
+					<li key={operator} className={aOperator} onClick={onInput}>
 						{operator}
 					</li>
 				)}
